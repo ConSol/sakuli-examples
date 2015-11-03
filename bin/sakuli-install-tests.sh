873194ca98dcac88be02a37ac1a22930671a90af
Simon Meggle <simon.emggle@consol.de>
2015-11-03 15:55:37 +0100

    added updater script for sakuli-examples.

--- bin/sakuli-install-tests.sh
+++ bin/sakuli-install-tests.sh
@@ -1,14 +1,66 @@
 #!/bin/bash
+clear 
+GITURL="git@github.com:ConSol/sakuli-examples.git"
 
-$GIT=$(which git)
+GIT=$(which git)
 if [ ! -x "$GIT" ]; then 
 	echo "ERROR: GIT binary not found."
 	exit 1; 
 fi
-if [ ! -d "$SAKULI_HOME" ]; then 
-	echo "ERROR: $SAKULI_HOME does not exist!"
+
+# remove last slash
+SAKULI_HOME=$(echo "$SAKULI_HOME" | sed 's#/$##')
+SAKULI_INST=${SAKULI_HOME%/*}
+if [ ! -d "$SAKULI_INST" ]; then 
+	echo "ERROR: Could not determine SAKULI_INST from SAKULI_HOME ($SAKULI_HOME)!"
 	exit 1
 fi
 
+if [ ! -d $SAKULI_INST/sakuli-examples ]; then
+	echo "> sakuli-examples not found; trying to clone from GitHub... "
+	$GIT clone $GITURL
+	if [ -d $SAKULI_INST/sakuli-examples ]; then 
+		echo -n " done."
+	else
+		echo -n "FAILED. Exting."
+		exit 1
+	fi
+fi 
 
-SAKULI_INST=$SAKULI_HOME/../
+cd $SAKULI_INST/sakuli-examples
+
+echo "Searching for changes on both sides..."
+
+if (($(git status --porcelain | egrep "^(\?| ?M)" | wc -l))); then 
+	echo "Working copy is dirty (uncommitted changes). Commit and push first or remve the file(s). Leaving untouched and exiting. Details:"
+	$GIT status
+	exit 0
+fi
+
+$GIT remote update
+
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse @{u})
+BASE=$(git merge-base @ @{u})
+
+# local - stage - remote
+if [ $LOCAL = $REMOTE ]; then
+	echo "> Working copy is up-to-date ($LOCAL)."
+elif [ $REMOTE = $BASE ]; then
+	echo "> Working copy contains committed changes. Push first, if possible, or reset everything with 'git reset --hard'. Leaving untouched and exiting. Details:"
+	$GIT diff origin/master
+	exit 0
+elif [ $LOCAL = $BASE ]; then
+	echo "> Working copy is out of date. Pulling from GitHub..."
+	$GIT pull 
+	LOCAL=$(git rev-parse @)
+	REMOTE=$(git rev-parse @{u})
+	if [ ! $LOCAL = $REMOTE ]; then
+		echo " failed. Exiting."
+		exit 1
+	else 
+		echo " successful."
+		echo "> Working copy is up-to-date ($LOCAL)."
+	fi
+fi
+
