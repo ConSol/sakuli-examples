9c4ade38499e11113d31c4406252706103911b59
simonmeggle <simon.emggle@consol.de>
2015-11-06 15:06:08 +0100

    Added force-update switch

--- bin/sakuli-testloop.sh
+++ bin/sakuli-testloop.sh
@@ -4,8 +4,20 @@ export DISPLAY=:0.0
 source ~/.bashrc
 DIR=`dirname $0`
 RUNFILE=$DIR/../suites.run
-RUNFILE=${1-$RUNFILE}
 
+numargs=$#
+for ((i=1 ; i <= numargs ; i++))
+do
+	case $1 in
+		"--force-sakuli-update")
+			FORCE_SAKULI_UPDATE=$1
+			;;
+		"--force-test-update")
+			FORCE_TEST_UPDATE=$1
+			;;
+	esac
+    	shift
+done
 
 
 if [ ! -r "$RUNFILE" ]; then
@@ -13,11 +25,11 @@ if [ ! -r "$RUNFILE" ]; then
 	exit 1
 fi
 
-$DIR/sakuli-install-latest.sh --force-update
-#$DIR/sakuli-install-tests.sh
+#$DIR/sakuli-install-latest.sh $FORCE_SAKULI_UPDATE
+#$DIR/sakuli-install-tests.sh $FORCE_TEST_UPDATE
 
-while read suite; do 
+for suite in $(cat $RUNFILE | grep -v '^#'); do 
 	echo $suite
 	$SAKULI_HOME/bin/sakuli.sh --run $SAKULI_HOME/../sakuli-examples/$suite --vnc
-done <$RUNFILE
+done 
 
